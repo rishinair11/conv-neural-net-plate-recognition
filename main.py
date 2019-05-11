58b85d7319780a17b5c7bd6089916e925f638a78
Rishikesh Nair <alienware505@gmail.com>
2019-05-11 13:47:38 +0530

    updated main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import Start
+import os
 import PIL
 import mysql.connector
 from mysql.connector import Error
@@ -9,7 +10,6 @@ try:
                                          password='')
     if connection.is_connected():
         db_Info = connection.get_server_info()
-        print("Connected to MySQL database... MySQL Server version on ", db_Info)
 
         # Get the row with case id = 0  TABLE : cases
         cursor = connection.cursor()
@@ -23,35 +23,35 @@ try:
             print("Reporter id : ", reporterid)
             image_list.append([caseid, licenseplatepic])
 
+        platenumber = None
         # Get neural network prediction
         for image in image_list:
             # preprocess image
-            with open('{}.jpg'.format(image[0]), 'wb') as file:
+            path = '{}.jpg'.format(image[0])
+            with open(path, 'wb') as file:
                 file.write(image[1])
-            platenumber = Start.recognize('{}.jpg'.format(image[0]))
+            platenumber = Start.recognize(path)
+            # delete file to save storage
+            os.remove(path)
 
-        platenumber = 'CHIGGA 47'
-        platetuple = (platenumber,)
+            platetuple = (platenumber,)
 
-        # Get the row with vehicle number to get offender's id TABLE : vehicleregistration
-        cursor2 = connection.cursor()
-        query2 = ("SELECT id FROM vehicleregistration "
-                  "WHERE licenseplateno = %s")
+            # Get the row with vehicle number to get offender's id TABLE : vehicleregistration
+            cursor2 = connection.cursor()
+            query2 = ("SELECT id FROM vehicleregistration "
+                      "WHERE licenseplateno = %s")
 
-        cursor2.execute(query2, (platetuple))
-        for (id) in cursor2:
-            print("Case id : ", id)
-            criminalid = id[0]
+            cursor2.execute(query2, (platetuple))
+            for (id) in cursor2:
+                print("Case id : ", id)
+                criminalid = id[0]
 
-        # print(case_id)
-        # print(criminalid)
-
-        # Update the offender's id TABLE : cases
-        cursor3 = connection.cursor()
-        query3 = """ UPDATE cases
-                   SET aadharid = %s
-                   WHERE caseid = %s """
-        cursor3.execute(query3, (criminalid, case_id))
+            # Update the offender's id TABLE : cases
+            cursor3 = connection.cursor()
+            query3 = """ UPDATE cases
+                    SET aadharid = %s
+                    WHERE caseid = %s """
+            cursor3.execute(query3, (criminalid, image[0]))
 
 
 except Error as e:
